62d9b7a99fbc4c785ee2c42d383c1db09b410b00
Daniel Demidko <demidko@farpost.com>
2020-08-05 12:14:35 +1000

    Применяю ФП

--- src/main/kotlin/State.kt
+++ src/main/kotlin/State.kt
@@ -1,10 +1,8 @@
 data class State(val left: Place, val right: Place = emptySet())
 
-fun State.next() =
-  (left.mapNotNull { it.move(left, right) } + right.mapNotNull { it.move(right, left) } + this)
-    .toSet()
+fun State.next() = left.mapNotNull { it.move(left, right) } + this.right.mapNotNull { it.move(right, left) } + this
 
-fun Set<State>.evolution(): Unit = when (val next = this.also(::println).map(State::next).flatten().toSet()) {
+fun Set<State>.evolution(): Unit = when (val next = also(::println).map(State::next).flatten().toSet()) {
   this -> Unit
   else -> next.evolution()
 }
